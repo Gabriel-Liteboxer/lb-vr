fb8099a002f0685f2a973acb92e583e65b81f57a
Andrew Rollins <51332383+AndrewRollins1@users.noreply.github.com>
2019-07-08 10:18:58 -0400

    Refined Game States

---
+++ LiteBoxVR/Assets/FacePlayerPromptUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacePlayerPromptUI : MonoBehaviour
+{
+    static Transform PlayerHead;
+
+    public Vector3 PositionOffset;
+
+    public float LerpSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(PlayerHead == null)
+        {
+            PlayerHead = GameObject.FindGameObjectWithTag("PlayerHead").transform;
+
+        }
+
+        transform.forward = Vector3.forward;
+
+        transform.position = Vector3.Lerp(transform.position, PlayerHead.transform.position + PositionOffset, LerpSpeed*Time.deltaTime);
+
+        transform.forward = PlayerHead.forward;
+
+        
+
+    }
+}
--- LiteBoxVR/Assets/GameManager.cs
+++ LiteBoxVR/Assets/GameManager.cs
@@ -5,12 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : TagModularity
 {
-    public string EnvironmentScene;
-
-    public string SongSelectScene;
-
-    public string ControllerModeSelectScene;
-
+    
     public enum GameState
     {
         controllerModeSelect,
@@ -24,39 +19,123 @@ public class GameManager : TagModularity
     }
 
 
-
+    [Header("Current State of Game")]
     public GameState StateOfGame;
 
+    [System.Serializable]
     public class GameStateScene
     {
+        public GameState AssociatedState;
+        public string SceneName;
 
+        public bool Loading;
+        public bool Loaded;
+
+        public string CallOnLoaded;
+
+        public bool SetActiveOnLoaded;
 
     }
 
+    [Header("Game State Scenes To Load")]
+    public GameStateScene[] gameStateScene;
 
-    public bool LoadingSongSelect;
+    Dictionary<GameState, GameStateScene> GameStateDict = new Dictionary<GameState, GameStateScene>();
 
+    /*
+    [Header("Scene Names")]
+    public string EnvironmentScene;
+    public string SongSelectScene;
+    public string ControllerModeSelectScene;
+    public string GamePlayScene;
+
+
+    [Header("Scene Loading Status")]
     public bool LoadingControllerSelect;
+    public bool LoadingControllerCalibration;
+    public bool LoadingEnvironment;
+    public bool LoadingSongSelect;
+    public bool LoadingGamePlay;*/
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(LoadEnvironment());
+        foreach(GameStateScene gss in gameStateScene)
+        {
+            GameStateDict.Add(gss.AssociatedState, gss);
+
+        }
+
+        //StartCoroutine(LoadEnvironment());
     }
 
 
     private void Update()
     {
-        if(StateOfGame == GameState.songSelect)
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            StateOfGame++;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Z))
+        {
+            StateOfGame--;
+
+        }
+
+        /*
+        if(GameStateDict.ContainsKey(StateOfGame))
+        {
+            if(!GameStateDict[StateOfGame].Loading)
+            {
+                StartCoroutine(LoadGameStateScene(GameStateDict[StateOfGame]));
+
+            }
+            else if (GameStateDict[StateOfGame].Loaded)
+            {
+                SendMessage(GameStateDict[StateOfGame].CallOnLoaded);
+
+            }
+
+        }
+        */
+        /*
+        if (StateOfGame == GameState.songSelect)
         {
             if(!LoadingSongSelect)
             StartCoroutine(LoadSongSelect());
 
-        }
+        }*/
+
+        LoadSceneFromGameState(StateOfGame);
 
         
     }
 
+    public bool LoadSceneFromGameState(GameState aState)
+    {
+        if (GameStateDict.ContainsKey(aState))
+        {
+            if (!GameStateDict[aState].Loading && !GameStateDict[aState].Loaded)
+            {
+                StartCoroutine(LoadGameStateScene(GameStateDict[aState]));
+
+            }
+            else if (GameStateDict[aState].Loaded)
+            {
+                if(GameStateDict[aState].CallOnLoaded != "")
+                SendMessage(GameStateDict[aState].CallOnLoaded);
+
+            }
+
+            return true;
+
+        }
+
+        return false;
+    }
+
+    /*
     IEnumerator LoadControllerModeSelect()
     {
         LoadingControllerSelect = true;
@@ -108,5 +187,26 @@ public class GameManager : TagModularity
         SceneManager.SetActiveScene(SceneManager.GetSceneByName(EnvironmentScene));
 
         Debug.Log("loaded environment");
+    }
+    */
+    IEnumerator LoadGameStateScene(GameStateScene aScene)
+    {
+        aScene.Loading = true;
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(aScene.SceneName, LoadSceneMode.Additive);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        if (aScene.SetActiveOnLoaded)
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName(aScene.SceneName));
+
+        aScene.Loading = false;
+        aScene.Loaded = true;
+
+        Debug.Log("loaded " + aScene.SceneName);
     }
 }
