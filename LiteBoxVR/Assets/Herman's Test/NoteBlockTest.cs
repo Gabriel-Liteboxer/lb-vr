cedf638072847db9473e62839108c8ab0577fb1d
Hermanlejter <hermanlejter@gmail.com>
2019-10-25 17:38:52 -0400

    worked out a quick demo of potential note visuals

---
+++ LiteBoxVR/Assets/Herman's Test/NoteBlockTest.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteBlockTest : MonoBehaviour
+{
+
+    [SerializeField] private Material m_NormalMaterial;
+    [SerializeField] private float m_NoteMoveSpeed;
+    [SerializeField] private float m_NoteDissolveSpeed;
+    [SerializeField] private GameObject m_NoteExplosion;
+    private Material m_DissolveMaterial;
+
+    private float m_DissolveAmount = 0;
+    private bool m_SolidMatSet = false;
+
+    private float m_DestroyTimer = .5f;
+
+
+    private void Awake()
+    {
+        m_DissolveMaterial = GetComponent<Renderer>().material;
+        m_DissolveMaterial.SetFloat("_DissolveAmount", 1);
+        iTween.MoveTo(gameObject, iTween.Hash("position", new Vector3(transform.position.x, transform.position.y, transform.position.z + .8f), "time", m_NoteMoveSpeed, "easetype", iTween.EaseType.easeInExpo));
+
+    }
+
+    void Update()
+    {
+        m_DissolveAmount = m_DissolveMaterial.GetFloat("_DissolveAmount");
+        Debug.Log(m_DissolveAmount);
+        if (m_DissolveAmount > .005f)
+        {
+            m_DissolveMaterial.SetFloat("_DissolveAmount", Mathf.Lerp(m_DissolveAmount, 0, Time.deltaTime * m_NoteDissolveSpeed));
+        }
+        else if (!m_SolidMatSet)
+        {
+            m_SolidMatSet = true;
+            GetComponent<Renderer>().material = m_NormalMaterial;
+        }
+        if (m_SolidMatSet)
+        {
+            m_DestroyTimer -= Time.deltaTime;
+        }
+        if (m_DestroyTimer <= 0)
+        {
+            Instantiate(m_NoteExplosion, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ LiteBoxVR/Assets/Herman's Test/NotePartentTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotePartentTest : MonoBehaviour
+{
+    [SerializeField] private GameObject m_Note;
+
+    [SerializeField] private float m_TimerMax;
+
+    private float m_CurrentTIme;
+
+    private Transform m_NewNotePos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_CurrentTIme > 0)
+        {
+            m_CurrentTIme -= Time.deltaTime;
+        }
+        else
+        {
+            // timer logic - every "timer max" seconds, this logic executes
+
+            m_CurrentTIme = m_TimerMax;
+            Vector3 randomizedPos = new Vector3(transform.position.x + Random.Range(-.5f, .5f), transform.position.y + Random.Range(-.5f, .5f), transform.position.z - .5f);
+            Instantiate(m_Note, randomizedPos, Quaternion.identity);
+
+        }
+    }
+}
